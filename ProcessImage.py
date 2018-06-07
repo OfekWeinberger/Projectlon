3a23560af912e201c548124392d91f36396af6f1
Ofek Weinberger <ofekwa@gmail.com>
2018-06-07 16:08:29 +0300

    hi

--- ProcessImage.py
+++ ProcessImage.py
@@ -16,3 +16,26 @@ def close_holes(mask):
     erd = cv2.erode(src=dil, kernel=kernel, iterations=5)
 
     return erd
+
+
+def blob():
+    # Read image
+    im = cv2.imread("raw_footage\\1.jpeg", cv2.IMREAD_GRAYSCALE)
+    cv2.imshow("Keypoints", im)
+    cv2.waitKey(0)
+    # Set up the detector with default parameters.
+    detector = cv2.SimpleBlobDetector()
+
+    # Detect blobs.
+    keypoints = detector.detect(im)
+
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Show keypoints
+    cv2.imshow("Keypoints", im_with_keypoints)
+    cv2.waitKey(0)
+
+
+blob()

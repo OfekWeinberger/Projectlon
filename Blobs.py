1c88a6543ef80615cf468f1e8e93b31e688c900a
Ofek Weinberger <ofekwa@gmail.com>
2018-06-07 17:21:20 +0300

    hi

--- Blobs.py
+++ Blobs.py
@@ -3,21 +3,36 @@ import cv2
 import numpy as np;
 
 # Read image
-im = cv2.imread("raw_footage\\3.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.imread("raw_footage\\4.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Keypoints", im)
 cv2.waitKey(0)
 # Set up the detector with default parameters.
-detector = cv2.SimpleBlobDetector()
 
-# Detect blobs.
-is_v2 = cv2.__version__.startswith("2.")
-if is_v2:
-    detector = cv2.SimpleBlobDetector()
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Filter by threshold
+params.minThreshold = 200
+params.maxThreshold = 255
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 0.01
+
+# Filter by color.
+params.filterByColor = True
+params.blobColor = 255
+
+# Create a detector with the parameters
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3:
+    detector = cv2.SimpleBlobDetector(params)
 else:
-    detector = cv2.SimpleBlobDetector_create()
+    detector = cv2.SimpleBlobDetector_create(params)
+
 
 keypoints = detector.detect(im)
-print(keypoints)
+print(keypoints[0].pt)
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)

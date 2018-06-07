f1a8b34152f708fdb88511f1af83ea43202106de
Ofek Weinberger <ofekwa@gmail.com>
2018-06-07 14:52:17 +0300

    hi

--- Main.py
+++ Main.py
@@ -2,16 +2,19 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+fgbg = cv2.createBackgroundSubtractorKNN(history=3)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    fgmask = fgbg.apply(frame)
+
+    # grayscale:
+    #gray = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', fgmask)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 

c03c3522c17ac94e13a5e2c7391de8d2e59539af
Ofek Weinberger <ofekwa@gmail.com>
2018-06-08 03:07:45 +0300

    hi

---
+++ ArduinoConnect.py
@@ -0,0 +1,11 @@
+import serial
+import time
+
+
+
+def laser_on():
+    ser = serial.Serial("COM7", 9600)
+    time.sleep(1.5)
+    ser.write(b"1")
+
+laser_on()
--- raw_footage/ArduinoConnect.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-
-def laser_on():
-    ser = serial.Serial('/dev/tty.usbserial', 9600)
-    ser.write(b"1")
-
-
-laser_on()
